2f482ecf96106d811a160cbcb0c7a06b09ec6444
resid <frpblcccnt@gmail.com>
2022-08-21 01:56:06 -0700

    Revert "First Version1"
    This reverts commit 897b4a2dc480cd089ec2d79eb7a583e03222de68.

--- src/main/java/newsApp/entity/NewsEntity.java
+++ src/main/java/newsApp/entity/NewsEntity.java
@@ -20,6 +20,9 @@ public class NewsEntity {
     @Column(name = "body")
     private String body;
 
+//    @Column(name = "typeId")
+//    private String typeId;
+
     @ManyToOne
     @JoinColumn(name="typeId",insertable = false)
     private TypeEntity type;
